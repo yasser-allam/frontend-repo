10e4c7b26c8d5fa59cefafe9b6ddb3ea912ce84c
yasser allam <yasser-allam@outlook.com>
2024-07-07 14:33:17 +0300

    added code to client.py

--- client.py
+++ client.py
@@ -1,1 +1,6 @@
-
+class Client:
+    def __init__(self, server_ip):
+        self.server_ip = server_ip
+    
+    def connect_to_server(self):
+        print(f"Connecting to server at {self.server_ip}")
