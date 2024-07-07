ed63fca019b6f112a20287c4e07130c0ffe3da39
yasser allam <yasser-allam@outlook.com>
2024-07-08 00:34:45 +0300

    test

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
+import yaml
+
 class Client:
-    def __init__(self, server_ip):
-        self.server_ip = server_ip
+    def __init__(self):
+        with open("../config.yaml", "r") as config_file:
+            config = yaml.safe_load(config_file)
+            self.server_ip = config["ServerIPAddress"]
     
     def connect_to_server(self):
         print(f"Connecting to server at {self.server_ip}")
